8f214db692bfc58df3d722d96e67599f54937ac0
neclord02 <amglez@correo.ugr.es>
2015-12-01 13:45:28 +0100

    Vista de los eventos y cursos terminada
    html y css sin mas

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,96 +1,26 @@
 $(document).ready(function(){
 
-	$( '#desc_empresa_1' ).mouseenter(
+	$( "[ id^=desc_empresa_ ]" ).mouseenter(
+	
 		function(){
-			$( '#desc_empresa_1' ).css({
+			
+			var num = this.id.slice(13);
+			
+			$( '#desc_empresa_' + num ).css({
+				
 				'opacity' : '0.6'
+				
 			});
 		}
 	);
 	
-	$( '#desc_empresa_1' ).mouseleave(
-		function(){
-			$( '#desc_empresa_1' ).css({
-				'opacity' : '0.0'
-			});
-		}
-	);
+	$( "[ id^=desc_empresa_ ]" ).mouseleave(
 	
-	$( '#desc_empresa_2' ).mouseenter(
 		function(){
-			$( '#desc_empresa_2' ).css({
-				'opacity' : '0.6'
-			});
-		}
-	);
-	
-	$( '#desc_empresa_2' ).mouseleave(
-		function(){
-			$( '#desc_empresa_2' ).css({
-				'opacity' : '0.0'
-			});
-		}
-	);
-	
-	$( '#desc_empresa_3' ).mouseenter(
-		function(){
-			$( '#desc_empresa_3' ).css({
-				'opacity' : '0.6'
-			});
-		}
-	);
-	
-	$( '#desc_empresa_3' ).mouseleave(
-		function(){
-			$( '#desc_empresa_3' ).css({
-				'opacity' : '0.0'
-			});
-		}
-	);
-	
-	$( '#desc_empresa_4' ).mouseenter(
-		function(){
-			$( '#desc_empresa_4' ).css({
-				'opacity' : '0.6'
-			});
-		}
-	);
-	
-	$( '#desc_empresa_4' ).mouseleave(
-		function(){
-			$( '#desc_empresa_4' ).css({
-				'opacity' : '0.0'
-			});
-		}
-	);
-	
-	$( '#desc_empresa_5' ).mouseenter(
-		function(){
-			$( '#desc_empresa_5' ).css({
-				'opacity' : '0.6'
-			});
-		}
-	);
-	
-	$( '#desc_empresa_5' ).mouseleave(
-		function(){
-			$( '#desc_empresa_5' ).css({
-				'opacity' : '0.0'
-			});
-		}
-	);
-	
-	$( '#desc_empresa_6' ).mouseenter(
-		function(){
-			$( '#desc_empresa_6' ).css({
-				'opacity' : '0.6'
-			});
-		}
-	);
-	
-	$( '#desc_empresa_6' ).mouseleave(
-		function(){
-			$( '#desc_empresa_6' ).css({
+			
+			var num = this.id.slice(13);
+			
+			$( '#desc_empresa_' + num ).css({
 				'opacity' : '0.0'
 			});
 		}
